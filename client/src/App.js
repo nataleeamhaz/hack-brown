31237c6d8ff8d909cba20159905c1e1ca3271cd8
skarnati20 <73152445+skarnati20@users.noreply.github.com>
2022-01-29 12:34:33 -0500

    a

--- client/src/App.js
+++ client/src/App.js
@@ -12,21 +12,13 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+      </head>
+      <div className="container">
+        <h1>This is the site.</h1>
+      </div>
     </div>
   );
 }
